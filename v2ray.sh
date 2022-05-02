c1abe5c320e422de9f94bd4bd34adb2cab0c326f
luoyinhuan <102496001+luoyinhuan@users.noreply.github.com>
2022-05-02 12:51:33 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -273,7 +273,7 @@ getData() {
             CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
             KEY_FILE="/etc/v2ray/${DOMAIN}.key"
         else
-            resolve=`curl -sL https://api64.ipify.org/d=${DOMAIN}`
+            resolve=`curl -sL https://api64.ipify.org/？d=${DOMAIN}`
             res=`echo -n ${resolve} | grep ${IP}`
             if [[ -z "${res}" ]]; then
                 colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
